8231a06fbf0f139dd96b10457db08b7c3e848713
Jay Bowers <jay.bowers@forgerock.com>
2019-04-11 17:00:59 +0100

    Add echo of request elements

--- echo/echo.go
+++ echo/echo.go
@@ -4,12 +4,36 @@ import (
     "fmt"
     "log"
     "net/http"
+    "io"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
+    log.Println(r)
+    if len(r.Header.Get("X-Echo")) > 1 {
+        fmt.Fprintf(w, "header = %s\n", r.Header.Get("X-Echo"))
+    }
     if len(r.URL.Path) > 1 {
         fmt.Fprintf(w, "path = %s\n", r.URL.Path[1:])
     }
+    if len(r.URL.RawQuery) > 1 {
+        fmt.Fprintf(w, "query = %s\n", r.URL.RawQuery)
+    }
+    if len(r.URL.Fragment) > 1 {
+        fmt.Fprintf(w, "fragment = %s\n", r.URL.Fragment)
+    }
+    hasBody := []string{"POST","PUT","PATCH"}
+    if in(r.Method, hasBody) {
+        io.Copy(w, r.Body)
+    }
+}
+
+func in(a string, list []string) bool {
+    for _, b := range list {
+        if b == a {
+            return true
+        }
+    }
+    return false
 }
 
 func main() {
